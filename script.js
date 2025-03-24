6a4ed5a65b2e57576727e9337a9fbd74751dd256
m0hdsameer <124045961+m0hdsameer@users.noreply.github.com>
2025-03-24 21:35:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const headerText = document.querySelector("header h1");
+    const scrollDown = document.querySelector(".scroll-down");
+
+    headerText.classList.add("fade-in");
+    scrollDown.classList.add("fade-in");
+});
